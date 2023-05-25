95db01a61e9f228821dcb7b44f8280415f59a5be
Ductham08 <ductham784@gmail.com>
2023-05-26 01:36:09 +0700

    up

--- database/kidsschool.sql
+++ database/kidsschool.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2023 at 04:10 PM
+-- Generation Time: May 25, 2023 at 08:35 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -20,6 +20,52 @@ SET time_zone = "+00:00";
 --
 -- Database: `kidsschool`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `id_class` int(11) NOT NULL,
+  `class_name` varchar(100) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `tuition` varchar(15) NOT NULL,
+  `desc_class` text NOT NULL,
+  `capacity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`id_class`, `class_name`, `start_date`, `end_date`, `tuition`, `desc_class`, `capacity`) VALUES
+(1, 'Lớp vẽ', '2023-05-03', '2023-05-18', '250000', 'moo tasdasd\r\nadasd\r\nasd\r\nasd\r\nasd\r\nasd\r\nasd\r\nasd\r\nasd\r\nasd\r\nasd\r\nasd\r\nasdas\r\nd', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id_student` int(11) NOT NULL,
+  `birthday` date NOT NULL,
+  `student_name` varchar(50) NOT NULL,
+  `phone_farther` varchar(11) NOT NULL,
+  `phone_morther` varchar(11) NOT NULL,
+  `id_class` int(11) NOT NULL,
+  `day_admission` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id_student`, `birthday`, `student_name`, `phone_farther`, `phone_morther`, `id_class`, `day_admission`) VALUES
+(2, '2023-04-26', ' Trần Minh Nhật ', '0967550072', '876541230', 1, '2023-05-10');
 
 -- --------------------------------------------------------
 
@@ -41,6 +87,18 @@ CREATE TABLE `user` (
 --
 
 --
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`id_class`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id_student`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -49,6 +107,18 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+  MODIFY `id_class` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id_student` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
